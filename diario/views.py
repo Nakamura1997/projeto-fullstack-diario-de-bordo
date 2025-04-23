7908b617ced2df7d24fb0b7ea6cf65c600647b3a
Nakamura1997 <joaovictor.oliveira11@gmail.com>
2025-04-23 16:42:17 -0300

    #17 add() Criar Views

--- diario/views.py
+++ diario/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from rest_framework import generics
+from .models import RegistroDiario
+from .serializers import RegistroDiarioSerializer
 
-# Create your views here.
+class RegistroListCreateView(generics.ListCreateAPIView):
+    queryset = RegistroDiario.objects.all()
+    serializer_class = RegistroDiarioSerializer
+
+class RegistroDetailView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = RegistroDiario.objects.all()
+    serializer_class = RegistroDiarioSerializer

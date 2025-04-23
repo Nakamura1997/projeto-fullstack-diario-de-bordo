c4ad24861f6a1fa3d38c1fb4da8698e55af9891e
Nakamura1997 <joaovictor.oliveira11@gmail.com>
2025-04-23 16:37:59 -0300

    #15 Add() Definir o Modelo de Dados

--- diario/models.py
+++ diario/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+class RegistroDiario(models.Model):
+    titulo = models.CharField(max_length=200)
+    conteudo = models.TextField()
+    data_criacao = models.DateTimeField(default=timezone.now)
+    data_atualizacao = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.titulo
